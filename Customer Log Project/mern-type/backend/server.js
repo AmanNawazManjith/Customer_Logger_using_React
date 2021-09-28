b4449b1bac10e2e12e05931929a1269b04d8d92b
Aman Nawaz Manjith <71788604+AmanNawazManjith@users.noreply.github.com>
2021-09-28 11:34:06 +0530

    t  Q# Please enter the commit message for your changes. Lines starting

--- Customer Log Project/mern-type/backend/server.js
+++ Customer Log Project/mern-type/backend/server.js
@@ -1,10 +1,21 @@
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
+const path = require('path');
 
 require('dotenv').config();
 
 const app = express();
+
+//Server static assets if in production
+if(process.env.NODE_ENV === 'production'){
+    //Set static folder
+    app.use(express.static('Customer Log Project/mern-type/backend/build'));
+
+    app.get('*', (req, res) =>{
+        res.sendFile(path.resolve(__dirname,'backend','build','index.html'));
+    });
+}
 const port = process.env.PORT || 5000;
 
 app.use(cors());
