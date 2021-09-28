ffec13f28a6bbd57a545ea46db8ade4606667578
Aman Nawaz Manjith <71788604+AmanNawazManjith@users.noreply.github.com>
2021-09-28 13:58:44 +0530

    files edited

--- server.js
+++ server.js
@@ -10,13 +10,13 @@ const app = express();
 //Server static assets if in production
 if(process.env.NODE_ENV === 'production'){
     //Set static folder
-    app.use(express.static(path.join(__dirname, '../frontend/build')));
+    app.use(express.static('frontend/build'));
 
-  app.get('*', (req, res) =>
-    res.sendFile(
-      path.resolve(__dirname, '..', 'frontend', 'build', 'index.js')
-    )
-  );
+    app.get('*', (request, response) => {
+
+        response.sendFile(path.join(__dirname, 'frontend/build', 'index.html'));
+        
+         });
 }
 const port = process.env.PORT || 5000;
 
