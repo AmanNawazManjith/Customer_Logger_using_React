ff1d49dd634ea65f7667b8b8ab6bb19fec095420
Aman Nawaz Manjith <71788604+AmanNawazManjith@users.noreply.github.com>
2021-10-01 11:53:39 +0530

    Edited Files

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import * as serviceWorker from './serviceWorker';
 // import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
@@ -10,4 +11,4 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-
+serviceWorker.unregister();
